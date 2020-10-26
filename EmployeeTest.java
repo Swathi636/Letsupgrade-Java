2c60a0f9b7261dd94a08aad512d7e1c94621b9de
Swathi636 <61228967+Swathi636@users.noreply.github.com>
2020-10-26 16:26:28 +0530

    Create EmployeeTest.java

---
+++ EmployeeTest.java
@@ -0,0 +1,13 @@
+
+public class EmployeeTest {
+
+   public static void main(String args[]) {
+ 
+      Employee emp = new Employee();
+     
+      emp.empAge(23);
+      emp.empName("Saurabh");
+      emp.empCity("Chennai");
+      emp.printEmployee();
+   }
+}
