7f03a8a1d3c2fdc318c6fb6aa22d2d515c9a3112
Swathi636 <61228967+Swathi636@users.noreply.github.com>
2020-11-01 14:16:38 +0530

    Day-3

--- Employee.java
+++ Employee.java
@@ -1,26 +1,37 @@
+package com.Letsupgrade;
 public class Employee {
 
+    String name;
+    int day,month,year;
+    double mSalary,aSalary,taxAmount;
 
-			   String name;
-			   int age;
-			   String city;
+    public void display(){
 
-			   public void empName(String empName) {
-			      name = empName;
-			   }
+        System.out.println("\nEmployee Name: " + name);
+        System.out.println("Employee DOB: " + day + "-" + month + "-" + year);
+        System.out.println("Age: " + (2020-year));
+        aSalary = mSalary * 12;
+        System.out.println("Annual Salary: " + aSalary);
 
-			   public void empAge(int empAge) {
-			      age = empAge;
-			   }
+        if (aSalary>=500000) {
+            taxAmount = aSalary * 0.2;
+            System.out.println("Tax Amount: " + taxAmount); //20% tax
+        }
+        else if(aSalary>=400000 && aSalary <500000){
+            taxAmount = aSalary * 0.15;
+            System.out.println("Tax Amount: " + taxAmount); //15% tax
+        }
+        else if(aSalary>=300000 && aSalary <400000){
+            taxAmount = aSalary * 0.1;
+            System.out.println("Tax Amount: " + taxAmount); //10% tax
+        }
+        else if(aSalary>=200000 && aSalary <300000){
+            taxAmount = aSalary * 0.05;
+            System.out.println("Tax Amount: " + taxAmount); //5% tax
+        }
+        else if(aSalary<200000){
+            System.out.println("Salary is less than 2 lacs.");
+        }
 
-			   public void empCity(String empCity) {
-			      city = empCity;
-			   }
-
-			   public void printEmployee() {
-			      System.out.println("Name:"+ name );
-			      System.out.println("Age:" + age );
-			      System.out.println("City:" + city );
-
-			   }
-			}
+    }
+}
