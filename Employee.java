9803321395d373f9169e3709aef847cc323c1060
Swathi636 <61228967+Swathi636@users.noreply.github.com>
2020-11-01 14:15:08 +0530

    Day-2

--- Employee.java
+++ Employee.java
@@ -21,7 +21,6 @@ public class Employee {
 			      System.out.println("Name:"+ name );
 			      System.out.println("Age:" + age );
 			      System.out.println("City:" + city );
-			      
+
 			   }
 			}
-		
